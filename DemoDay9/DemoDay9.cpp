997cdea0457cc39d631931a82b27cd982772d443
Karl <krohr@fullsail.com>
2025-05-28 13:51:55 -0400

    Second Commit

---
+++ DemoDay9/DemoDay9.cpp
@@ -0,0 +1,42 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main()
+{
+    std::cout << "Day 9";
+
+    // References
+    // Pattern
+    // dataType &varName = otherVarname
+
+    // First we need a starter variable to show this off
+    // We're going to make an int
+    int age = 43;
+
+    int& ageRef = age; // So keep in mind that a reference, references another variable and once you change the value of the reference
+    // it should change the value of the referenced variable
+
+    std::cout << "age: " << age << std::endl;
+
+    ageRef = 11;
+
+    std::cout << "age: " << age << std::endl;
+
+    // Version Control - Git with Github
+
+    // Git
+
+
+    // Git hub 
+    // 
+    // https://www.github.com
+
+    // https://github.com/settings/education/benefits
+    
+    // Possibly File IO
+
+}
+
+
